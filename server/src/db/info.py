ef56d8d2d5f9fcb76a66f88447863a88957743d3
Jake Mensch <jake.mensch@gmail.com>
2020-06-04 15:03:57 -0700

    deleted unnecessary f-strings in db module

--- server/src/db/info.py
+++ server/src/db/info.py
@@ -2,14 +2,14 @@ from .conn import exec_sql
 
 
 def create_table():
-    exec_sql(f"""
+    exec_sql("""
         CREATE TABLE metadata AS
         SELECT * FROM (VALUES (NOW())) as vals(last_pulled)
     """)
 
 
 def update():
-    exec_sql(f"""
+    exec_sql("""
         UPDATE metadata SET last_pulled = NOW()
     """)
 
@@ -21,7 +21,7 @@ def last_updated():
 
 
 def years():
-    rows = exec_sql(f"""
+    rows = exec_sql("""
         SELECT DISTINCT date_part('year', createddate) as years
         FROM requests
     """)
@@ -29,7 +29,7 @@ def years():
 
 
 def rows():
-    res = exec_sql(f"""
+    res = exec_sql("""
         SELECT date_part('year', createddate) as year, COUNT(*)
         FROM requests
         GROUP BY year
--- server/src/db/reset.py
+++ server/src/db/reset.py
@@ -7,7 +7,7 @@ from utils.log import log
 def reset():
     log('\nResetting database')
 
-    exec_sql(f"""
+    exec_sql("""
         DROP TABLE IF EXISTS requests CASCADE;
         DROP TABLE IF EXISTS stage;
         DROP TABLE IF EXISTS metadata;
