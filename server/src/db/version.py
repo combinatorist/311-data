9f34ca192166b57e7bb278e897fa5de26747f1bb
Jake Mensch <jake.mensch@gmail.com>
2020-06-05 11:26:04 -0700

    rewrote db.version test to avoid error

--- server/src/db/version.py
+++ server/src/db/version.py
@@ -8,12 +8,21 @@ def version():
          0 ==> if metadata table doesn't include 'version'
        > 0 ==> the version in the metadata table
     '''
-    try:
-        meta = exec_sql('SELECT * FROM metadata LIMIT 1').first()
-    except Exception as e:
+
+    meta_exists = exec_sql("""
+        SELECT EXISTS (
+            SELECT FROM information_schema.tables
+            WHERE table_schema = 'public'
+            AND   table_name   = 'metadata'
+       )
+    """).first()[0]
+
+    if not meta_exists:
         return -1
+
+    meta = exec_sql('SELECT * FROM metadata LIMIT 1').first()
+
+    if not hasattr(meta, 'version'):
+        return 0
     else:
-        if not hasattr(meta, 'version'):
-            return 0
-        else:
-            return meta.version
+        return meta.version
